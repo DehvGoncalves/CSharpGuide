c9908cb4199791ccd6cf5f395761e98be94d5f64
Debora Gonçalves <71988020+deboragoncalves1@users.noreply.github.com>
2024-02-29 16:02:19 -0300

    69. ArrayList

---
+++ CursoCSharp/Colecoes/ColecoesArrayList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace CursoCSharp.Colecoes {
+    internal class ColecoesArrayList {
+        public static void Executar() {
+            var arrayList = new ArrayList {
+                "Palavra",
+                3,
+                true
+            };
+             
+            arrayList.Add(3.45);
+
+            Console.WriteLine("Usando o foreach");
+            foreach (var indic in arrayList) {
+                Console.WriteLine($" Para o indice {indic}");
+            }
+
+            Console.WriteLine("**************************************");
+            Console.WriteLine("Usando o for");
+            for (int i = 0; i < arrayList.Count; i++) {
+                Console.WriteLine($" O indice {i} é {arrayList[i]}");
+            }
+
+            Console.WriteLine("**************************************");
+            Console.WriteLine("Tipos de dados foreach e GetType");
+            foreach (var item in arrayList) {
+                Console.WriteLine("{0} => {1}", item, item.GetType());
+            }
+        }
+    }
+}
